0f340bd4c0fce4342cadf84829272e43bc3a5635
Joe Niemiec <josephxsxn@gmail.com>
2023-11-27 14:13:07 -0500

    Create explode_array_2_many_records.js

---
+++ code_snipplets/explode_array_2_many_records.js
@@ -0,0 +1,31 @@
+s = {
+  $source: {
+    connectionName: 'jsncluster0',
+    db: 'test',
+    coll: 'arrayunwind',
+    fullDocument: 'whenAvailable'
+  }
+}
+
+rr =       {
+  $replaceRoot: { newRoot : "$fullDocument"}
+}
+
+m = {$match : { 'data' : {$all: [0]}}}
+
+uw = {$unwind : '$data'}
+
+us = { $unset: "_id" }
+
+merge =  {$merge: {
+  into: {
+      connectionName: "jsncluster0",
+      db: "test",
+      coll: "arraytestdatamerge"},
+      on: ["data"],
+      whenMatched: "replace"
+
+}}
+
+db.arrayunwind.insertOne({'data' : [1,2,3,4,5,6,7,8,9,10] })
+db.arrayunwind.insertOne({'data' : [0,1,2,3,4,5,6,7,8,9,10] })
