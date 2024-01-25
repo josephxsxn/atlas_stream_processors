8ce656fd1f11d7e7cd23e33c12c838a6b9b55748
Joe Niemiec <josephxsxn@gmail.com>
2024-01-25 11:11:50 -0500

    Update pipelineAdder.js

--- example_processors/additive_merge/pipelineAdder.js
+++ example_processors/additive_merge/pipelineAdder.js
@@ -26,7 +26,9 @@ db.transactions.insertMany([
 {name : "joe",
 money : 100},
 {name : "jane",
-money : -10.50}
+money : -10.50},
+{name : "john",
+money : 55.55}
 ])
 
 s = {
@@ -52,7 +54,9 @@ rr =   {
       let : {value : "$money"},
       whenMatched : [ { $addFields: {
         money: { $add:[ "$money", "$$value" ] },
-      } }]
+      } }],
+     whenNotMatched: "insert"
+
 }}
 
 sp.process([s,rr,m])
@@ -60,14 +64,23 @@ sp.process([s,rr,m])
 /*
 [
   {
-    _id: ObjectId("65b2858e26d988e2c11ba3b0"),
+    _id: ObjectId("65b2880e26d988e2c11ba3b4"),
     name: 'joe',
     money: 110
   },
   {
-    _id: ObjectId("65b2858e26d988e2c11ba3b1"),
+    _id: ObjectId("65b2880e26d988e2c11ba3b5"),
     name: 'jane',
     money: 39.5
+  },
+  {
+    _id: ObjectId("65b2881226d988e2c11ba3b8"),
+    name: 'john',
+    money: 55.55,
+    _stream_meta: {
+      sourceType: 'atlas',
+      timestamp: ISODate("2024-01-25T16:10:58.669Z")
+    }
   }
 ]*/
 
