bad4627759a118f106807fc347e458ff6c28f616
Joe Niemiec <josephxsxn@gmail.com>
2024-02-01 12:39:05 -0500

    Update steps_add_multiplex_window.js

--- example_processors/additive_merge/steps_add_multiplex_window.js
+++ example_processors/additive_merge/steps_add_multiplex_window.js
@@ -129,3 +129,187 @@ db.steps_daily.find()
 db.steps_weekly.find()
 db.steps_yearly.find()
 db.steps_monthly.find()
+
+
+/* BELOW RESULTS DIDNT WAIT FOR THE LAST WINDOW TO CLOSE FOR THE LAST RECORD.
+    Atlas atlas-ec9c8m-shard-0 [primary] test> db.steps_daily.find()
+[
+  {
+    _id: ObjectId("65bbd57606169f8e7c8f6a33"),
+    steps: 968,
+    report_id: 'joe-1-12-2022',
+    user: 'joe',
+    reports: 'daily',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2022-12-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2022-12-01T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd57a06169f8e7c8f6a94"),
+    steps: 968,
+    report_id: 'joe-1-5-2023',
+    user: 'joe',
+    reports: 'daily',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-05-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-05-01T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd57c06169f8e7c8f6b05"),
+    steps: 968,
+    report_id: 'joe-1-12-2023',
+    user: 'joe',
+    reports: 'daily',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-12-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-12-01T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd58006169f8e7c8f6b65"),
+    steps: 968,
+    report_id: 'joe-29-12-2023',
+    user: 'joe',
+    reports: 'daily',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-12-29T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-12-29T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd58806169f8e7c8f6c68"),
+    steps: 1936,
+    report_id: 'joe-30-12-2023',
+    user: 'joe',
+    reports: 'daily',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-12-30T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-12-30T15:00:00.000Z")
+    }
+  }
+]
+Atlas atlas-ec9c8m-shard-0 [primary] test> db.steps_weekly.find()
+[
+  {
+    _id: ObjectId("65bbd57606169f8e7c8f6a28"),
+    steps: 968,
+    report_id: 'joe-482022',
+    user: 'joe',
+    reports: 'weekly',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2022-12-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2022-12-01T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd57a06169f8e7c8f6a9e"),
+    steps: 968,
+    report_id: 'joe-182023',
+    user: 'joe',
+    reports: 'weekly',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-05-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-05-01T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd57c06169f8e7c8f6b0f"),
+    steps: 968,
+    report_id: 'joe-482023',
+    user: 'joe',
+    reports: 'weekly',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-12-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-12-01T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd58006169f8e7c8f6b6f"),
+    steps: 2904,
+    report_id: 'joe-522023',
+    user: 'joe',
+    reports: 'weekly',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-12-29T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-12-29T15:00:00.000Z")
+    }
+  }
+]
+Atlas atlas-ec9c8m-shard-0 [primary] test> db.steps_monthly.find()
+[
+  {
+    _id: ObjectId("65bbd57606169f8e7c8f6a49"),
+    steps: 968,
+    report_id: 'joe-12-2022',
+    user: 'joe',
+    reports: 'monthly',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2022-12-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2022-12-01T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd57a06169f8e7c8f6ab2"),
+    steps: 968,
+    report_id: 'joe-5-2023',
+    user: 'joe',
+    reports: 'monthly',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-05-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-05-01T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd57c06169f8e7c8f6b22"),
+    steps: 3872,
+    report_id: 'joe-12-2023',
+    user: 'joe',
+    reports: 'monthly',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-12-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-12-01T15:00:00.000Z")
+    }
+  }
+]
+Atlas atlas-ec9c8m-shard-0 [primary] test> db.steps_yearly.find()
+[
+  {
+    _id: ObjectId("65bbd57606169f8e7c8f6a3e"),
+    steps: 968,
+    report_id: 'joe-2022',
+    user: 'joe',
+    reports: 'yearly',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2022-12-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2022-12-01T15:00:00.000Z")
+    }
+  },
+  {
+    _id: ObjectId("65bbd57a06169f8e7c8f6aa8"),
+    steps: 4840,
+    report_id: 'joe-2023',
+    user: 'joe',
+    reports: 'yearly',
+    _stream_meta: {
+      sourceType: 'atlas',
+      windowStartTimestamp: ISODate("2023-05-01T14:00:00.000Z"),
+      windowEndTimestamp: ISODate("2023-05-01T15:00:00.000Z")
+    }
+  }
+]
+    */
