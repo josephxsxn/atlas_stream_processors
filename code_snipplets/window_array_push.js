8cf7aab22a77a22ebb36aadeab4b754eca4e8e2b
Joe Niemiec <josephxsxn@gmail.com>
2024-02-09 15:11:08 -0500

    Update window_array_push.js

--- code_snipplets/window_array_push.js
+++ code_snipplets/window_array_push.js
@@ -1,14 +1,15 @@
-db.gpsCords.insertOne({ "vehicle_id": "vid0001", "coord": [37.76643495, -122.3969431 ] })
-db.gpsCords.insertOne({ "vehicle_id": "vid0001", "coord": [38.76643495, -123.3969431 ] })
-db.gpsCords.insertOne({ "vehicle_id": "vid0001", "coord": [39.76643495, -124.3969431 ] })
-db.gpsCords.insertOne({ "vehicle_id": "vid0001", "coord": [40.76643495, -125.3969431 ] })
+{ "vehicle_id": "vid0001", "coord": [37.76643495, -122.3969431 ], timestamp :  new ISODate("2022-12-01T14:10:30Z") }
+{ "vehicle_id": "vid0001", "coord": [38.76643495, -123.3969431 ], timestamp :  new ISODate("2022-12-01T14:10:31Z") }
+{ "vehicle_id": "vid0001", "coord": [39.76643495, -124.3969431 ] , timestamp :  new ISODate("2022-12-01T14:10:32Z") }
+{ "vehicle_id": "vid0001", "coord": [40.76643495, -125.3969431 ] , timestamp :  new ISODate("2022-12-01T14:10:33Z") }
+
 
 w = { 
     $tumblingWindow: {
       interval: {size: NumberInt(10), unit: "second"},
       idleTimeout : {size: NumberInt(5), unit: "second"},
       "pipeline": 
-      [ 
+      [ { $sort : {"_ts" : 1}},
         {
           '$group': {
             '_id': '$vehicle_id', 
@@ -30,8 +31,8 @@ db.gpsCord_res.find()
     _id: 'vid0001',
     _stream_meta: {
       sourceType: 'atlas',
-      windowStartTimestamp: ISODate("2024-02-09T18:23:00.000Z"),
-      windowEndTimestamp: ISODate("2024-02-09T18:23:10.000Z")
+      windowStartTimestamp: ISODate("2022-12-01T14:10:30.000Z"),
+      windowEndTimestamp: ISODate("2022-12-01T14:10:40.000Z")
     },
     positions: [
       { coord: [ 37.76643495, -122.3969431 ] },
@@ -41,3 +42,4 @@ db.gpsCord_res.find()
     ]
   }
 ]
+
