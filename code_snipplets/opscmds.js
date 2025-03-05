a40d684c404e4bc3eddda643e32330b7d1da3307
Joe Niemiec <josephxsxn@gmail.com>
2025-03-05 17:04:51 -0500

    Update opscmds.js

--- code_snipplets/opscmds.js
+++ code_snipplets/opscmds.js
@@ -1,3 +1,8 @@
 #fast recovery from lost resumeToken
 sp.name.modify({resumeFromCheckpoint: false})
 sp.name.start()
+
+
+#passing $date over REST or TF
+{"$source" : {"connectionName" : "jsncluster0", "db" : "test", "coll" : "dateTest",
+              "config" : {"startAtOperationTime" : {"$date": "2025-01-21T19:25:18.262Z"}}}}
