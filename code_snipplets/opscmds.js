80e741b121662898fa085213505e746eef6f6ec2
Joe Niemiec <josephxsxn@gmail.com>
2025-04-04 13:13:35 -0400

    Update opscmds.js

--- code_snipplets/opscmds.js
+++ code_snipplets/opscmds.js
@@ -6,3 +6,10 @@ sp.name.start()
 #passing $date over REST or TF
 {"$source" : {"connectionName" : "jsncluster0", "db" : "test", "coll" : "dateTest",
               "config" : {"startAtOperationTime" : {"$date": "2025-01-21T19:25:18.262Z"}}}}
+
+
+##Mod Processor Pipeline
+pipeline = sp.name.stats().pipeline
+pipeline[0]["$source"]["config"]["fullDocument"] = "whenAvailable"
+sp.name.modify(pipeline, {resumeFromCheckpoint: false})
+sp.name.start()
